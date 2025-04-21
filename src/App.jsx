49c8314ee46f4cc47ee7dc50f3166ff480c404f1
Ismael-Rvas <ismael.rivas.developer@gmail.com>
2025-04-21 12:39:47 +0200

    Añadir logo, instalar tailwind y ordenar estructura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-400 via-blue-500 to-purple-600">
+      <h1 className="text-4xl font-extrabold tracking-tight text-gray-100 mb-4">Solarium UI</h1>
+      <img src="https://res.cloudinary.com/ismaelrvas/image/upload/v1745231513/solariumLogo-removebg-preview_zjdfx8.png" alt="solariumLogo" className="w-32 h-32 rounded-full shadow-lg" />
+    </div>
   )
 }
 
