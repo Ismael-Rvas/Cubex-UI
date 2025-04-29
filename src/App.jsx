6ed39ca296ff4c16b8285e86057be2f4c4bb551a
Ismael-Rvas <ismael.rivas.developer@gmail.com>
2025-04-29 13:03:05 +0200

    Loader, icon page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { ThemeProvider } from "../src/context/ThemeContext";
 import { TranslationProvider } from "../src/context/TranslationContext";
 import { MyRoutes } from "./routes/routes";
-import { Suspense } from "react";
+import { useState, useEffect } from "react";
+import LazyLoader from "./components/components/UI/loader/LazyLoader";
 
 function App() {
-  return (
-    <Suspense fallback={<div className="bg-white dark:bg-black pt-36 md:pt-60">Loading...</div>}>
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer); 
+  }, []);
+
+  if (loading) {
+    return <LazyLoader />;
+  }
+
+  return(
       <ThemeProvider>
         <TranslationProvider>
           <MyRoutes />
         </TranslationProvider>
       </ThemeProvider>
-    </Suspense>
   );
 }
 
---
+++ src/components/components/UI/loader/LazyLoader.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import Loader from "./Loader";
+import { ThemeProvider } from "../../../../context/ThemeContext";
+
+const LazyLoader = ({ children }) => {
+    const [loading, setLoading] = useState(true);
+    const [fadeOut, setFadeOut] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFadeOut(true);
+            setTimeout(() => setLoading(false), 5000);
+        }, 1700);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return loading ? (
+        <ThemeProvider>
+            <div className={`fixed inset-0 flex items-center justify-center bg-white dark:bg-black transition-opacity duration-500 ease-in-out`}>
+                <Loader />
+            </div>
+        </ThemeProvider>
+        ) : (
+        children
+    );
+};
+
+export default LazyLoader;
